b46547d0be609e8933986cb44922ffc1a4ad6a7e
entrezunfredici <superfred2468@gmail.com>
2022-03-08 10:55:47 +0100

    AddBootstrap

--- ProgetCapouSymfony/assets/app.js
+++ ProgetCapouSymfony/assets/app.js
@@ -4,9 +4,28 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
-// any CSS you import will output into a single css file (app.css in this case)
+// CSS files
 import './styles/app.css';
-
+//bootstrap
+import './bootstrap/bootstrap.min.css';
+// JS files
+import './controllers/hello_controller';
+//boostrap
+import './bootstrap/bootstrap.bundle.min';
+//JQuery
+import './controllers/jquery-3.3.1.slim.min';
 // start the Stimulus application
 import './bootstrap';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
