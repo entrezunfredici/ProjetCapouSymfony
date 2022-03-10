5b4a95f57b9474e090a9a0e168fa645a7fd6d6b5
entrezunfredici <superfred2468@gmail.com>
2022-03-10 12:56:09 +0100

    mobile update

--- ProgetCapouSymfony/assets/controllers/move.js
+++ ProgetCapouSymfony/assets/controllers/move.js
@@ -109,16 +109,6 @@
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
-
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
-
   /**
    * Mobile nav dropdowns activate
    */
